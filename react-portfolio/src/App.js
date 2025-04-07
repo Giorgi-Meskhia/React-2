ac7b556b046449d14c877430bf086054df863cd0
Giorgi-Meskhia <giomesxo08@gmail.com>
2025-04-07 22:55:06 +0400

    homework

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="profile.png" className="profile-image" alt="My Profile" />
+        <h1>გიორგი მესხია</h1>
+        <p>ცოდნა: HTML, CSS, JavaScript და PHP. ვმუშაობ ვებ დეველოპმენტის მიმართულებით.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
